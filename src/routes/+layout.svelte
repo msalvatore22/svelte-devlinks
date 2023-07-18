5bf32093a5930b5bbaf3776f66ae476495f264f5
Michael Salvatore <msalvatore22@gmail.com>
2023-07-18 11:24:58 -0400

    Set up custom font-family

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import "../app.css"
+    
 </script>
 
 <slot />
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;600;700&display=swap');
+</style>
